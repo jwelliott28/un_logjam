64c1e7d92ef3bbfffe010ae2fb6598c19099a02e
Jared Elliott <jwelliott28@gmail.com>
2021-02-03 22:48:45 -0500

    Updated log_gen.py to create a log file with it's name based on the date

--- log_gen.py
+++ log_gen.py
@@ -2,15 +2,33 @@
 
 import os
 from pathlib import Path
+import datetime
 
-os.chdir(Path('/home/jared/projects/programming/py_scripts/radius_clear/path_to_test_files'))
+#Create a datetime object
+dt = datetime.datetime.now()
+
+#Create a day, month and year attribute
+day = dt.day
+month = dt.month
+year = dt.year
+
+#Change path to where logs will go
+
+os.chdir(Path('/home/jared/projects/programming/net_automation/py_scripts/radius_clear/path_to_test_files'))
+
+#Verify you are in the correct path
 
 print(Path.cwd())
 
-for i in range(1, 32):
+#Create a for loop that generates a file with the date as it's title
+
+#for i in range(1, 32):
 
     #Create a file for each iteration
     #Remember i must be converted to a string with str()
 
-    testFile = open('Dec_'+str(i)+'_2020.txt', 'w')
+    #testFile = open('Dec_'+str(i)+'_2020.txt', 'w')
 
+#Create a log with the name based on the day's date
+
+log = open(str(month)+'_'+str(day)+'_'+str(year)+'.txt', 'w')
